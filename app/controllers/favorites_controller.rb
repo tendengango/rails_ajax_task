4ddc0e8919925d4d582cc599ba4fa6625f45ccbd
tendengango <framboutel@gmail.com>
2023-04-14 19:45:23 +0100

    rails_ajax_task

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+  before_action :set_blog
+
+  def create
+    current_user.favorites.create(blog_id: params[:blog_id])
+  end
+
+  def destroy
+    current_user.favorites.find_by(id: params[:id]).destroy
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:blog_id])
+  end
+end
